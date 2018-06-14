e6e15d5127e7b77aacc7388e3545fc8004fa6050
yongseok kang <yongseok.kang@navercorp.com>
2018-06-14 09:48:26 +0900

    Initial Commit

---
+++ LearningOpenCV2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningOpenCV2
+//
+//  Created by KangYongseok on 14/06/2018.
+//  Copyright © 2018 feelform Corp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
