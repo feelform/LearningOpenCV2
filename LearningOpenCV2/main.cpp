627cf7b23577d68d39e1a0266b54edd75ebc5108
yongseok kang <yongseok.kang@navercorp.com>
2018-06-14 11:10:27 +0900

    Init project

--- LearningOpenCV2/main.cpp
+++ LearningOpenCV2/main.cpp
@@ -7,9 +7,16 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cv::Mat img = cv::imread(argv[1], -1);
+    if (img.empty()) {
+        return -1;
+    }
+    cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Example1", img);
+    cv::waitKey(0);
+    cv::destroyWindow("Example1");
     return 0;
 }
