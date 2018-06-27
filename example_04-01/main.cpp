47f6f1050fb18001eb8f7870ce45d1f6900f243a
Yongseok Kang <feelform@gmail.com>
2018-06-27 23:53:38 +0900

    example 04

---
+++ example_04-01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example_04-01
+//
+//  Created by Yongseok Kang on 27/06/2018.
+//  Copyright © 2018 feelform Corp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
