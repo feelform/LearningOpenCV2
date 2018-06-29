9d5a3f9fb0ab99147fc06831dacedc59fa51490c
yongseok kang <yongseok.kang@navercorp.com>
2018-06-29 11:13:05 +0900

    Add example	04-02

---
+++ example_04-01/example_04-02.cpp
@@ -0,0 +1,22 @@
+//
+//  example_04-02.cpp
+//  example_04-01
+//
+//  Created by KangYongseok on 29/06/2018.
+//  Copyright © 2018 feelform Corp. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "\nExample 4-2. Summation of two arrays using the N-ary operator"
+        << "\nCall:\n"
+        << argv[0] << endl;
+    
+    
+    return 0;
+}
